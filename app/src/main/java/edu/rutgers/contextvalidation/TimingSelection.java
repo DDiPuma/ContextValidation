f8a6ef4a27d871a67400a0b920ea4d3d349244ed
Dominic DiPuma <dominic.dipuma@rutgers.edu>
2017-11-06 14:35:58 -0500

    Initial commit

---
+++ app/src/main/java/edu/rutgers/contextvalidation/TimingSelection.java
@@ -0,0 +1,17 @@
+package edu.rutgers.contextvalidation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TimingSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timing_selection);
+
+        // Set up GUI stuff
+    }
+
+    // Deal with persistent job schedule settings
+}
